940e5aeb7cb167da35031f9591585507ad2d7cec
toantran980 <jokemaking90@gmail.com>
2025-11-04 21:08:15 -0800

    implement digit cnn

--- skeleton/L04_digit_CNN.py
+++ skeleton/L04_digit_CNN.py
@@ -20,7 +20,12 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         # TODO: Make your convolutional, pooling, flattening, and fully connected layers here
-    
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        # Match the existing forward's hard-coded flatten size (1440)
+        self.fc1 = nn.Linear(1440, 120)
+        self.fc2 = nn.Linear(120, 10)
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = x.view(-1, 1440)
